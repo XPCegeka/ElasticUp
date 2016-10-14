726059cb0b74f760c1a1a99f0dec5ca4edd4dfcd
Boon Tom <tom.boon@cegeka.com>
2016-10-14 10:51:49 +0200

    [tom,yoram] ElasticUpRunner unit test

--- ElasticUp/ElasticUp.Tests/Runner/ElasticUpRunnerTest.cs
+++ ElasticUp/ElasticUp.Tests/Runner/ElasticUpRunnerTest.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿
+using System;
+using ElasticUp.Runner;
+using ElasticUp.Tests.Migration;
+using Nest;
+using NUnit.Framework;
 
 namespace ElasticUp.Tests.Runner
 {
@@ -6,10 +11,21 @@ namespace ElasticUp.Tests.Runner
     public class ElasticUpRunnerTest
     {
         [Test]
-        public void WhenAddingAMigration_()
+        public void WhenAddingAMigration_MakeSureAllMigrationNumbersAreUnique()
         {
-            
+            Assert.Throws<InvalidOperationException>(() =>
+                ElasticUpRunner.CreateElasticUpRunner(new ElasticClient())
+                    .Migration(new TestMigration(1))
+                    .Migration(new TestMigration(1)));
         }
 
+        [Test]
+        public void WhenAddingAMigration_MakeSureAllMigrationNumbersAreAscending()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+                ElasticUpRunner.CreateElasticUpRunner(new ElasticClient())
+                    .Migration(new TestMigration(2))
+                    .Migration(new TestMigration(1)));
+        }
     }
 }
