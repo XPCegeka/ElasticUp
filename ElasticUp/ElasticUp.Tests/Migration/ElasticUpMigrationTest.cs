b25d78e93ee6e480f129abe11882c6b8f491f7fb
YoramKerckhofs <yoram.kerckhofs@gmail.com>
2016-10-14 11:44:43 +0200

    Create ElasticUpMigration + tests

---
+++ ElasticUp/ElasticUp.Tests/Migration/ElasticUpMigrationTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using ElasticUp.Migration;
+using ElasticUp.Operation;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ElasticUp.Tests.Migration
+{
+    [TestFixture]
+    public class ElasticUpMigrationTest
+    {
+        private ElasticUpMigration _elasticUpMigration;
+
+        [SetUp]
+        public void Setup()
+        {
+            _elasticUpMigration = new TestMigration(0);
+        }
+
+        [Test]
+        public void Operation_AddsOperationToOperations()
+        {
+            // GIVEN
+            var operation = new ElasticUpOperation(0);
+
+            // WHEN
+            _elasticUpMigration.Operation(operation);
+
+            // THEN
+            _elasticUpMigration.Operations.Should().HaveCount(1);
+        }
+
+        [Test]
+        public void Operation_ThrowsWhenAddingOperationWithSameIndex()
+        {
+            // GIVEN
+            var operation1 = new ElasticUpOperation(0);
+            var operation2 = new ElasticUpOperation(0);
+
+            // WHEN / THEN
+            _elasticUpMigration.Operation(operation1);
+            Assert.Throws<ArgumentException>(() => _elasticUpMigration.Operation(operation2), "Duplicate operation number.");
+        }
+
+        [Test]
+        public void Execute_ExecutesEachOperation()
+        {
+            // GIVEN
+            var operation1 = Substitute.For<ElasticUpOperation>(0);
+            var operation2 = Substitute.For<ElasticUpOperation>(1);
+
+            _elasticUpMigration.Operation(operation1);
+            _elasticUpMigration.Operation(operation2);
+
+            // WHEN
+            _elasticUpMigration.Execute();
+
+            // THEN
+            operation1.Received().Execute();
+            operation2.Received().Execute();
+        }
+    }
+}
--- ElasticUp/ElasticUp/Migration/ElasticUpMigration.cs
+++ ElasticUp/ElasticUp/Migration/ElasticUpMigration.cs
@@ -1,15 +1,37 @@
-﻿namespace ElasticUp.Migration
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ElasticUp.Operation;
+
+namespace ElasticUp.Migration
 {
     public abstract class ElasticUpMigration
     {
-        public int MigrationNumber { get; }
+        internal int MigrationNumber { get; }
+        internal List<ElasticUpOperation> Operations { get; }
 
-        protected ElasticUpMigration(int migrationNumber)
+        internal ElasticUpMigration(int migrationNumber)
         {
             MigrationNumber = migrationNumber;
+            Operations = new List<ElasticUpOperation>();
         }
 
-       
+        internal void Operation(ElasticUpOperation operation)
+        {
+            if(HasDuplicateOperationNumber(operation)) 
+                throw new ArgumentException("Duplicate operation number.");
 
+            Operations.Add(operation);
+        }
+
+        internal void Execute()
+        {
+            Operations.ForEach(o => o.Execute());
+        }
+
+        private bool HasDuplicateOperationNumber(ElasticUpOperation operation)
+        {
+            return Operations.Any(o => o.OperationNumber == operation.OperationNumber);
+        }
     }
 }
