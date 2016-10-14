84aacbc0aadd5eb2460f0eef76ad7a2fa36007e0
ChristopheV <christophe.vroonen@gmail.com>
2016-10-14 10:06:59 +0200

    Fix solution structure

---
+++ ElasticUp/ElasticUp.Tests.Infrastructure/ElasticSearchContainer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Management;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ElasticUp.Tests.Infrastructure
+{
+    public class ElasticSearchContainer : IDisposable
+    {
+        private readonly string _tempDirectory;
+        private readonly Process _esProcess;
+
+        public ElasticSearchContainer(Stream elasticSearchArchive)
+        {
+            _tempDirectory = GetTempDirectory();
+
+            using (elasticSearchArchive)
+            using(var elasticSearch = new ZipArchive(elasticSearchArchive))
+            {
+                elasticSearch.ExtractToDirectory(_tempDirectory);
+            }
+
+            _esProcess = StartElasticSearch();
+        }
+
+        public static ElasticSearchContainer StartNewFromArchive(byte[] elasticSearchArchive)
+        {
+            return new ElasticSearchContainer(new MemoryStream(elasticSearchArchive));
+        }
+
+        public void WaitUntilElasticOperational()
+        {
+            SpinWait.SpinUntil(() => IsElasticSearchUpAndRunning().Result);
+        }
+
+        public void Dispose()
+        {
+            KillProcessAndChildren(_esProcess.Id);
+            Directory.Delete(_tempDirectory, true);
+        }
+
+        private Process StartElasticSearch()
+        {
+            var esBinPath = Path.Combine(_tempDirectory, @"bin\elasticsearch.bat");
+            var processInfo = new ProcessStartInfo("cmd.exe", $"/c {esBinPath}")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            return Process.Start(processInfo);
+        }
+
+        private static async Task<bool> IsElasticSearchUpAndRunning()
+        {
+            using (var httpClient = new HttpClient())
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:9200"))
+            {
+                try
+                {
+                    var response = await httpClient.SendAsync(request);
+                    return response.IsSuccessStatusCode;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        private static string GetTempDirectory()
+        {
+            var tempFile = Path.GetTempFileName();
+            return Path.ChangeExtension(tempFile, null);
+        }
+
+        /// <summary>
+        /// Kill a process, and all of its children, grandchildren, etc.
+        /// </summary>
+        /// <param name="pid">Process ID.</param>
+        /// <sauce>http://stackoverflow.com/a/10402906</sauce>
+        private static void KillProcessAndChildren(int pid)
+        {
+            var searcher = new ManagementObjectSearcher
+              ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            var moc = searcher.Get();
+            foreach (var mo in moc.Cast<ManagementObject>())
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                var proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+        }
+    }
+}
---
+++ ElasticUp/ElasticUp.Tests/AbstractIntegrationTest.cs
@@ -0,0 +1,15 @@
+﻿using ElasticUp.Tests.Infrastructure;
+
+namespace ElasticUp.Tests
+{
+    public abstract class AbstractIntegrationTest
+    {
+        protected ElasticSearchContainer SetupElasticSearchService()
+        {
+            var esService = ElasticSearchContainer.StartNewFromArchive(Resources.elasticsearch_2_4_1);
+            esService.WaitUntilElasticOperational();
+
+            return esService;
+        }
+    }
+}
