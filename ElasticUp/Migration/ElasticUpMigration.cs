1d01773590d857b3f172ecaa92387b0e2fdc2dc3
Boon Tom <tom.boon@cegeka.com>
2016-10-14 10:01:49 +0200

    [tom,yoram] runner

---
+++ ElasticUp/Migration/ElasticUpMigration.cs
@@ -0,0 +1,15 @@
+﻿namespace ElasticUp.Migration
+{
+    public abstract class ElasticUpMigration
+    {
+        private readonly int _migrationNumber;
+
+        protected ElasticUpMigration(int migrationNumber)
+        {
+            _migrationNumber = migrationNumber;
+        }
+
+       
+
+    }
+}
---
+++ ElasticUp/Runner/ElasticUpRunner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElasticUp.Migration;
+
+namespace ElasticUp.Runner
+{
+    public class ElasticUpRunner
+    {
+        public readonly IList<ElasticUpMigration> Migrations = new List<ElasticUpMigration>();
+
+        private ElasticUpRunner() {}
+
+        public static ElasticUpRunner Create()
+        {
+            return new ElasticUpRunner();
+        }
+
+        public ElasticUpRunner Migration(ElasticUpMigration migration)
+        {
+            Migrations.Add(migration);
+            return this;
+        }
+
+        public void Run()
+        {
+            //TODO 
+        }
+
+    }
+}
