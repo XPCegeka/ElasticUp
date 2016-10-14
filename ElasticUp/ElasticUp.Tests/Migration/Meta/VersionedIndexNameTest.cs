9f886352a865804e4b0d9833f58e9cdb0fc09e37
ChristopheV <christophe.vroonen@gmail.com>
2016-10-14 11:33:46 +0200

    Add VersionedIndexName, add index alias fluent API to ELasticUpMigration

---
+++ ElasticUp/ElasticUp.Tests/Migration/Meta/VersionedIndexNameTest.cs
@@ -0,0 +1,44 @@
+﻿using ElasticUp.Migration.Meta;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ElasticUp.Tests.Migration.Meta
+{
+    [TestFixture]
+    public class VersionedIndexNameTest
+    {
+        [Test]
+        public void CreateFromVersionedIndexName_ReturnsParsedVersionedIndexName()
+        {
+            var indexName = VersionedIndexName.CreateFromIndexName("test-v1");
+            indexName.Name.Should().Be("test");
+            indexName.Version.Should().Be(1);
+        }
+
+        [Test]
+        public void CreateFromVersionedIndexName_ReturnsVersion0ForNonVersionedIndexName()
+        {
+            var indexName = VersionedIndexName.CreateFromIndexName("cegeka-is-great");
+            indexName.Name.Should().Be("cegeka-is-great");
+            indexName.Version.Should().Be(0);
+        }
+
+        [Test]
+        public void ToString_ReturnsFormattedVersionedIndexName()
+        {
+            var indexName = new VersionedIndexName("test", 1);
+            indexName.ToString().Should().Be("test-v1");
+        }
+
+        [Test]
+        public void GetIncrementedVersion_ReturnsNextVersion()
+        {
+            var indexName = new VersionedIndexName("test", 1);
+            var nextIndexName = indexName.GetIncrementedVersion();
+
+            nextIndexName.Name.Should().Be("test");
+            nextIndexName.Version.Should().Be(2);
+            nextIndexName.ToString().Should().Be("test-v2");
+        }
+    }
+}
--- ElasticUp/ElasticUp/Migration/ElasticUpMigration.cs
+++ ElasticUp/ElasticUp/Migration/ElasticUpMigration.cs
@@ -1,15 +1,36 @@
-﻿namespace ElasticUp.Migration
+﻿using System;
+using ElasticUp.Migration.Meta;
+using Nest;
+
+namespace ElasticUp.Migration
 {
     public abstract class ElasticUpMigration
     {
         private readonly int _migrationNumber;
+        public string IndexAlias { get; protected set; }
 
         protected ElasticUpMigration(int migrationNumber)
         {
             _migrationNumber = migrationNumber;
         }
 
-       
+        public ElasticUpMigration OnIndexAlias(string indexAlias)
+        {
+            IndexAlias = indexAlias;
+            return this;
+        }
 
+        public void Execute(IElasticClient elasticClient)
+        {
+            var indicesForAlias = elasticClient.GetIndicesPointingToAlias(IndexAlias);
+
+            foreach (var indexForAlias in indicesForAlias)
+            {
+                var indexName = VersionedIndexName.CreateFromIndexName(indexForAlias);
+                var nextIndexName = indexName.GetIncrementedVersion();
+
+                // TODO execute operations for each index
+            }
+        }
     }
 }
